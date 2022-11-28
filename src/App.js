69520de850d21983e66b3f71d642bcd13319b3c6
Syed Saif Ali <sali12@i2cinc.com>
2022-11-28 14:32:23 +0500

    Bingo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import Grid from "./components/Grid";
 import {data} from "./components/Helper";
-
+import Bingo from "./components/Bingo";
 
 const App =()=> {
 
   return (
-    <div className="App">
-        <h4 className="text-center mt-5">Bingo App!</h4>
-        <Grid data={data}/>
-    </div>
+      <>
+          <div className="App" style={{height:"100vh", background: "linear-gradient(90deg, rgba(98,173,230,1) 0%, rgba(71,117,115,1) 100%)"}}>
+              <Bingo/>
+              <Grid data={data}/>
+          </div>
+      </>
   );
 }
 
---
+++ src/components/Bingo.js
@@ -0,0 +1,18 @@
+import '../App.css'
+
+const Bingo =()=> {
+
+    return (
+        <>
+            <div className="wrapper">
+                <span>B</span>
+                <span>I</span>
+                <span>N</span>
+                <span>G</span>
+                <span>O</span>
+            </div>
+        </>
+    );
+}
+
+export default Bingo;
